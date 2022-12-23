b96b62f709f51a2ea30161a2927534328176819b
gyansahoo666 <119865696+gyansahoo666@users.noreply.github.com>
2022-12-23 10:41:36 -0800

    Android project added

---
+++ Todolist/app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtData;
+    private TextView lblShow;
+    private Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtData = (EditText) findViewById(R.id.txtData);
+        lblShow = (TextView) findViewById(R.id.lblShow);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lblShow.setText(txtData.getText().toString());
+            }
+        });
+    }
+}
